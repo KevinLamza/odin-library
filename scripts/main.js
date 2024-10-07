d190b54fdcc21ab6bb9ddc36a6faed9275811cee
Kevin Lamza <148631894+KevinLamza@users.noreply.github.com>
2024-10-08 01:08:49 +0300

    Initial commit
    Add book constructor from previous exercise

--- scripts/main.js
+++ scripts/main.js
@@ -1,31 +1,20 @@
-// // alert("hello");
+// alert("hello");
 
-// function Book(title, author, pages, isRead) {
-//         this.title = title;
-//         this.author = author;
-//         this.pages = pages;
-//         this.isRead = isRead;
-//         this.info = function () {
-//                 return (`${this.title} by ${this.author}, ${this.pages} pages, ${this.isRead}.`);
-//         };
-// }
-
-// const book1 = new Book("Vom Junkie zum Millionär", "Montana Black", 420, "not read yet");
-// const book2 = new Book("Flinker Hase", "Carsten", 240, "read");
-
-// console.log(book1.info());
-// console.log(book2.info());
-
-
-function Player(name, marker) {
-        this.name = name;
-        this.marker = marker;
-        this.sayName = function() {
-                console.log(this.name)
+function Book(title, author, pages, isRead) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.isRead = isRead;
+        this.info = function () {
+                return (`${this.title} by ${this.author}, ${this.pages} pages, ${this.isRead}.`);
         };
 }
 
-const player1 = new Player('steve', 'X');
-const player2 = new Player('also steve', 'O');
-      player1.sayName(); // logs 'steve'
-      player2.sayName(); // logs 'also steve'
+const book1 = new Book("Vom Junkie zum Millionär", "Montana Black", 420, "not read yet");
+const book2 = new Book("Flinker Hase", "Carsten", 240, "read");
+const book3 = new Book("Sieglinde vom Chor", "Sieglinde", 130, "not read yet");
+
+
+console.log(book1.info());
+console.log(book2.info());
+console.log(book3.info();
